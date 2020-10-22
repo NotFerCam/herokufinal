92b72faec322afb7a0fafbdb80159d596dd019eb
NotFerCam <davidfercam69@gmail.com>
2020-10-22 13:44:00 +0200

    mod php

--- index.php
+++ index.php
@@ -3,15 +3,22 @@ echo'<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <form>
-        <input type="text" name="email"><br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="btn_login"><br>
-    </form>
+    <header>
+        <h1>Menu</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="menu/Privado1/tirada_dados.php">Tirada</a></li>
+            <li><a href="menu/Privado2/sumar_7.php">Suamr 7</a></li>
+            <li><a href="menu/Privado3/par_impar.php">Par / Impar</a></li>
+            <li><a>Acerca de</a></li>
+        </ul>
+    </nav>
 </body>
 </html>';
 ?>
